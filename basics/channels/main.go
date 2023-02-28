15b7e9746351228bc5a1f0fd6b9d89e58f7fa4d2
Bogdan-Pechounov <119967588+Bogdan-Pechounov@users.noreply.github.com>
2023-02-27 19:10:19 -0500

    channels

--- basics/channels/main.go
+++ basics/channels/main.go
@@ -2,8 +2,44 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-func main() {
+var wg = sync.WaitGroup{}
 
+func main1() {
+	ch := make(chan int)
+	for j := 0; j < 5; j++ {
+		wg.Add(2)
+		go func(ch <-chan int) {
+			i := <-ch
+			fmt.Println(i)
+			wg.Done()
+		}(ch)
+		go func(ch chan<- int) {
+			ch <- 42
+			wg.Done()
+		}(ch)
+	}
+	wg.Wait()
+}
+
+func main() {
+	ch := make(chan int, 50)
+	wg.Add(2)
+	go func(ch <-chan int) {
+		for i := range ch {
+			fmt.Println(i)
+		}
+		wg.Done()
+	}(ch)
+	go func(ch chan<- int) {
+		ch <- 42
+		ch <- 27
+		ch <- 34
+		close(ch)
+		wg.Done()
+	}(ch)
+
+	wg.Wait()
 }
